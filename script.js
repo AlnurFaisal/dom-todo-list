3adb02da2239439e3b251cedd7860eac81798cb1
Alnur_Faisal <alnurfaisal@outlook.com>
2018-03-05 16:11:34 +0800

    Update my todo page to allow adding of items dynamically and stike out items that are completed

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-tasks = ["buy milk", "eat dinner", "nail javascript"]
+var tasks = ["buy milk", "eat dinner", "nail javascript"];
+var list;
+var item;
+var counter = 0;
+var todoList = document.getElementById("todo-list");
+var form = document.getElementsByTagName("form")[0];
+var addlist = document.getElementById("addlist");
+
+
+for (var i = 0; i < tasks.length; i++) {
+    list = document.createElement("li");
+    list.textContent = tasks[i];
+    list.id = "list" + counter;
+    counter++;
+    todoList.appendChild(list);
+}
+
+
+todoList.addEventListener("click", function(e){
+    var mylist = document.getElementById(e.target.id);     
+    if(!mylist.classList.contains("done")){
+        mylist.classList.add("done");
+    } else {
+        mylist.classList.remove("done");
+    }
+});
+
+form.addEventListener("submit", function(e){
+   e.preventDefault();
+   item = addlist.value;
+   tasks.push(item);
+   list = document.createElement("li");
+   list.textContent = tasks[tasks.length - 1];
+   list.id = "list" + counter;
+   todoList.appendChild(list);
+   counter++;
+});
+
+
+
+
+
